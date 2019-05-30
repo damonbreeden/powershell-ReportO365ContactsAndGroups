fd4da50a260de604e7b628ce0f16eb04b6d3e96b
Damon Breeden <49255023+damonbreeden@users.noreply.github.com>
2019-05-30 16:14:01 -0500

    Initial Commit
    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # powershell-ReportO365ContactsAndGroups
+
+Quick and dirty script to report on Office 365 group membership where external contacts are part of a group.
+
+Can easily be modified to report all group members
--- Report-O365ContactsAndGroups.ps1
+++ Report-O365ContactsAndGroups.ps1
@@ -1,21 +1,21 @@
-﻿<#$UserCredential = Get-Credential
+﻿$UserCredential = Get-Credential
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
 
-Import-PSSession $Session -DisableNameChecking#>
+Import-PSSession $Session -DisableNameChecking
 
-$reportDir = "C:\Users\dbreeden\Documents\NCJW"
+$reportDir = "C:\Users\user\Documents\"
 
 $groups = Get-DistributionGroup
 
 foreach ($g in $groups) {
-    $members = Get-DistributionGroupMember -Identity $g.Alias | where RecipientType -eq MailContact
+    $members = Get-DistributionGroupMember -Identity $g.Alias | Where-Object RecipientType -eq MailContact
     foreach ($m in $members) {
         $name = $g.Name
-        $o = $m | select DisplayName,ExternalEmailAddress
+        $o = $m | Select-Object DisplayName,ExternalEmailAddress
         Export-Csv -LiteralPath "$reportDir\$Name.csv" -InputObject $o -Append -NoTypeInformation
         }
     }
 
-$contacts = Get-MailContact | select DisplayName,ExternalEmailAddress
+$contacts = Get-MailContact | Select-Object DisplayName,ExternalEmailAddress
 
 $contacts | Export-Csv -LiteralPath "$reportDir\contacts.csv" -NoTypeInformation
