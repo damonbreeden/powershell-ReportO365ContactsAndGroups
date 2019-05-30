83e449947d2d947fad8fcc7f5bf64aa82e3c098a
Damon Breeden <49255023+damonbreeden@users.noreply.github.com>
2019-05-30 16:10:04 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# powershell-ReportO365ContactsAndGroups
---
+++ Report-O365Groups.ps1
@@ -0,0 +1,21 @@
+﻿<#$UserCredential = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+
+Import-PSSession $Session -DisableNameChecking#>
+
+$reportDir = "C:\Users\dbreeden\Documents\NCJW"
+
+$groups = Get-DistributionGroup
+
+foreach ($g in $groups) {
+    $members = Get-DistributionGroupMember -Identity $g.Alias | where RecipientType -eq MailContact
+    foreach ($m in $members) {
+        $name = $g.Name
+        $o = $m | select DisplayName,ExternalEmailAddress
+        Export-Csv -LiteralPath "$reportDir\$Name.csv" -InputObject $o -Append -NoTypeInformation
+        }
+    }
+
+$contacts = Get-MailContact | select DisplayName,ExternalEmailAddress
+
+$contacts | Export-Csv -LiteralPath "$reportDir\contacts.csv" -NoTypeInformation
